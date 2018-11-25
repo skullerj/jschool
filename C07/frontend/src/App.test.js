d8e181d9c358b0ad7c6a0f0f38a108857e6b67b3
Juan Rosero <jfrosero@udlanet.ec>
2018-11-25 12:06:00 -0500

    Fix App tests

--- C07/frontend/src/App.test.js
+++ C07/frontend/src/App.test.js
@@ -3,11 +3,22 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import App from './App.jsx'
-
+import { books } from './components/__tests__/fixtures.json'
 const app = shallow(<App />)
 
 describe('App tests', () => {
-
+  it('state.searchTerms should be updated using updateSearch terms method', () => {
+    expect(app.state('searchTerms')).toEqual('')
+    app.instance().updateSearchTerms('Searching books')
+    expect(app.state('searchTerms')).toEqual('Searching books')
+    app.instance().updateSearchTerms('')
+    expect(app.state('searchTerms')).toEqual('')
+  })
+  it('filterBooks should work correctly', () => {
+    const result = app.instance().filterBooks(books, 'Eloquent JavaScript')
+    expect(result.length).toEqual(1)
+    expect(result[0].title).toEqual('Eloquent JavaScript, 3rd Edition')
+  })
   describe('Nav drawer opening and closing', () => {
     it('should toggle state.showNav when toggleNav is called', () => {
       expect(app.state('showNav')).toEqual(false)
@@ -22,23 +33,6 @@ describe('App tests', () => {
       app.setState({ showNav: true })
       expect(app.state('showNav')).toEqual(true)
       expect(app.find('div.drawer-background').length).toEqual(1)
-    })
-  })
-
-  describe('Search terms text', () => {
-    it('state.searchTerms should be updated using updateSearch terms method', () => {
-      expect(app.state('searchTerms')).toEqual('')
-      app.instance().updateSearchTerms('Searching books')
-      expect(app.state('searchTerms')).toEqual('Searching books')
-      app.instance().updateSearchTerms('')
-      expect(app.state('searchTerms')).toEqual('')
-    })
-    it('should show and hide a div.search-terms element accordingly', () => {
-      expect(app.state('searchTerms')).toEqual('')
-      expect(app.find('div.search-terms').length).toEqual(0)
-      app.instance().setState({ searchTerms: 'Searching books' })
-      expect(app.state('searchTerms')).toEqual('Searching books')
-      expect(app.find('div.search-terms').length).toEqual(1)
     })
   })
 })
