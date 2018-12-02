3587b6436b04c11b47a505c557640213ae7662dd
Juan Rosero <jfrosero@udlanet.ec>
2018-12-02 12:06:23 -0500

    Fix Bookshelf tests

--- C08/frontend/src/components/__tests__/Bookshelf.test.js
+++ C08/frontend/src/components/__tests__/Bookshelf.test.js
@@ -1,49 +1,35 @@
 /* global describe, it, expect */
 
 import React from 'react'
-import { shallow } from 'enzyme'
-import Bookshelf from '../Bookshelf.jsx'
+import { mount } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom'
 import Book from '../Book'
-import BookDetails from '../BookDetails'
+import Bookshelf from '../Bookshelf'
 import { books } from './fixtures.json'
 
-const shelf = shallow(<Bookshelf books={books} />)
-
+const router = mount(
+  <MemoryRouter>
+    <Bookshelf books={books} />
+  </MemoryRouter>
+)
+const shelf = router.find(Bookshelf).at(0)
 describe('Bookshelf tests', () => {
   it('should render normally', () => {
     expect(shelf.instance()).toBeDefined()
   })
   it('should render a Book component for each book', () => {
-    shelf.update()
     expect(shelf.find(Book).length).toEqual(books.length)
   })
-  describe('showing book details', () => {
+  describe('redirecting to book page', () => {
     let testEvent = {
       preventDefault: () => {},
       stopPropagation: () => {}
     }
-    it('should set state.selectedBook when a Book is clicked', () => {
+    it('should redirect the user to a the book page when a book is clicked', () => {
       const book = shelf.find(Book).at(0)
       expect(book).toBeDefined()
       book.prop('onBookClick')(testEvent)
-      expect(shelf.state('selectedBook')).toEqual(books[0].id)
-    })
-    it('should set state.selectedBook when a book is hovered', () => {
-      const book = shelf.find(Book).at(0)
-      expect(book).toBeDefined()
-      book.prop('onBookPointerEnter')(testEvent)
-      expect(shelf.state('selectedBook')).toEqual(books[0].id)
-    })
-    it('should reset state.selectedBook when a book is hovered', () => {
-      const book = shelf.find(Book).at(0)
-      expect(book).toBeDefined()
-      book.prop('onBookPointerLeave')(testEvent)
-      expect(shelf.state('selectedBook')).toEqual(null)
-    })
-    it('should render a BookDetails component when selectedBook is defined', () => {
-      shelf.setState({ selectedBook: books[0].id })
-      shelf.update()
-      expect(shelf.find(BookDetails).length).toEqual(1)
+      expect(router.instance().history.location.pathname === `/books/${books[0].id}`)
     })
   })
 })
