cfdf71da6868015c0d87332abfc5c0dde3987e9e
Juan Rosero <jfrosero@udlanet.ec>
2018-12-05 14:28:15 -0500

    Added Input element

--- C09/frontend/src/App.jsx
+++ C09/frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import injectSheet from 'react-jss'
 
 import Nav from './components/Nav'
 import Header from './components/Header'
+import Input from './components/Input'
 
 const styles = theme => ({
   main: {
@@ -25,6 +26,10 @@ const styles = theme => ({
         templateRows: '80px 1fr'
       }
     }
+  },
+  test: {
+    margin: 30,
+    width: 200
   }
 })
 
@@ -36,6 +41,9 @@ class App extends Component {
         <Header />
         <Nav />
         <section className={classes.content}>
+          <div className={classes.test}>
+            <Input icon={<i className='fas fa-user' />} errorMessage='No puedes' placeholder='Username' />
+          </div>
           <Switch>
             <Route path='/books' exact render={({ match }) => <h1>Welcome Everywhere {match.url}</h1>} />
             <Route path='/books/:location(quito|medellin|cartagena)' exact render={({ match }) => <h1>Welcome to {match.params.location}</h1>} />
