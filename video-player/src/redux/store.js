e69c4def9477d24a6ce408984f7733e195dce182
Juan Rosero <jfrosero@udlanet.ec>
2018-12-29 08:19:41 -0500

    Persist state on localstorage

--- video-player/src/redux/store.js
+++ video-player/src/redux/store.js
@@ -1,9 +1,35 @@
 import { createStore } from 'redux';
 import main from './reducers';
 
+const loadState = () => {
+  try {
+    const serializedData = localStorage.getItem('state');
+    if (serializedData === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedData);
+  } catch (error) {
+    return undefined;
+  }
+};
+const saveState = state => {
+  try {
+    let serializedData = JSON.stringify(state);
+    localStorage.setItem('state', serializedData);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
 const store = createStore(
   main,
+  loadState(),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
